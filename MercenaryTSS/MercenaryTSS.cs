5bf4f674a9c7734ca2687b3c249b4d5dfd073078
Wayne Miller <wayne.c.miller@comcast.net>
2023-08-08 21:44:25 -0500

    Dump junk.

--- MercenaryTSS/MercenaryTSS.cs
+++ MercenaryTSS/MercenaryTSS.cs
@@ -1,11 +1,7 @@
 ﻿using Sandbox.Definitions;
-using Sandbox.Engine.Utils;
 using Sandbox.Game.Entities;
-using Sandbox.Game.Entities.Cube;
-using Sandbox.Game.EntityComponents;
 using Sandbox.Game.GameSystems.TextSurfaceScripts;
 using Sandbox.ModAPI;
-using SpaceEngineers.Game.ModAPI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -91,20 +87,10 @@ namespace BMC.MercenaryTSS
         Vector2 right;
         Vector2 newLine;
 
-        //List<IMyBatteryBlock> batteryBlocks = new List<IMyBatteryBlock>();
-        //List<IMyPowerProducer> windTurbines = new List<IMyPowerProducer>();
-        //List<IMyPowerProducer> hydroenEngines = new List<IMyPowerProducer>();
-        //List<IMySolarPanel> solarPanels = new List<IMySolarPanel>();
-        //List<IMyReactor> reactors = new List<IMyReactor>();
-        //List<IMyGasTank> tanks = new List<IMyGasTank>();
-
         List<IMyInventory> inventorys = new List<IMyInventory>();
         List<VRage.Game.ModAPI.Ingame.MyInventoryItem> inventoryItems = new List<VRage.Game.ModAPI.Ingame.MyInventoryItem>();
 
-        //Dictionary<string, cargoItemType> cargoOres = new Dictionary<string, cargoItemType>();
         Dictionary<string, cargoItemType> cargoIngots = new Dictionary<string, cargoItemType>();
-        //Dictionary<string, cargoItemType> cargoComponents = new Dictionary<string, cargoItemType>();
-        //Dictionary<string, cargoItemType> cargoItems = new Dictionary<string, cargoItemType>();
 
         VRage.Collections.DictionaryValuesReader<MyDefinitionId, MyDefinitionBase> myDefinitions;
         MyDefinitionId myDefinitionId;
@@ -129,7 +115,6 @@ namespace BMC.MercenaryTSS
         {
             base.Dispose(); // do not remove
             TerminalBlock.OnMarkForClose -= BlockMarkedForClose;
-
             // Called when script is removed for any reason, so that you can clean up stuff if you need to.
         }
 
@@ -224,44 +209,10 @@ namespace BMC.MercenaryTSS
             var myCubeGrid = myTerminalBlock.CubeGrid as MyCubeGrid;
             var myFatBlocks = myCubeGrid.GetFatBlocks().Where(block => block.IsWorking);
 
-            //batteryBlocks.Clear();
-            //windTurbines.Clear();
-            //hydroenEngines.Clear();
-            //solarPanels.Clear();
-            //reactors.Clear();
             inventorys.Clear();
-            //tanks.Clear();
 
             foreach (var myBlock in myFatBlocks)
             {
-                //if (myBlock is IMyBatteryBlock)
-                //{
-                //    batteryBlocks.Add((IMyBatteryBlock)myBlock);
-                //}
-                //else if (myBlock is IMyPowerProducer)
-                //{
-                //    if (myBlock.BlockDefinition.Id.SubtypeName.Contains("Wind"))
-                //    {
-                //        windTurbines.Add((IMyPowerProducer)myBlock);
-                //    }
-                //    else if (myBlock.BlockDefinition.Id.SubtypeName.Contains("Hydrogen"))
-                //    {
-                //        hydroenEngines.Add((IMyPowerProducer)myBlock);
-                //    }
-                //    else if (myBlock is IMyReactor)
-                //    {
-                //        reactors.Add((IMyReactor)myBlock);
-                //    }
-                //    else if (myBlock is IMySolarPanel)
-                //    {
-                //        solarPanels.Add((IMySolarPanel)myBlock);
-                //    }
-                //}
-                //else if (myBlock is IMyGasTank)
-                //{
-                //    tanks.Add((IMyGasTank)myBlock);
-                //}
-
                 if (myBlock.HasInventory)
                 {
                     for (int i = 0; i < myBlock.InventoryCount; i++)
@@ -271,10 +222,7 @@ namespace BMC.MercenaryTSS
                 }
             }
 
-            //cargoOres.Clear();
             cargoIngots.Clear();
-            //cargoComponents.Clear();
-            //cargoItems.Clear();
 
             foreach (var inventory in inventorys)
             {
@@ -292,13 +240,6 @@ namespace BMC.MercenaryTSS
 
                     var myType = new cargoItemType { item = item, amount = 0 };
 
-                    //if (type == "Ore")
-                    //{
-                    //    if (!cargoOres.ContainsKey(name))
-                    //        cargoOres.Add(name, myType);
-
-                    //    cargoOres[name].amount += amount;
-                    //}
                     if (type == "Ingot")
                     {
                         if (!cargoIngots.ContainsKey(name))
@@ -306,20 +247,6 @@ namespace BMC.MercenaryTSS
 
                         cargoIngots[name].amount += amount;
                     }
-                    //else if (type == "Component")
-                    //{
-                    //    if (!cargoComponents.ContainsKey(name))
-                    //        cargoComponents.Add(name, myType);
-
-                    //    cargoComponents[name].amount += amount;
-                    //}
-                    //else
-                    //{
-                    //    if (!cargoItems.ContainsKey(name))
-                    //        cargoItems.Add(name, myType);
-
-                    //    cargoItems[name].amount += amount;
-                    //}
                 }
             }
 
@@ -332,132 +259,10 @@ namespace BMC.MercenaryTSS
             newLine = new Vector2(0, 30 * textSize);
             myDefinitions = MyDefinitionManager.Static.GetAllDefinitions();
 
-            //if (config.Get("Settings", "Tanks").ToBoolean())
-            //    DrawTanksSprite(ref myFrame, ref myPosition, mySurface);
-
-            //if (config.Get("Settings", "Solar").ToBoolean())
-            //    DrawSolarPanelSprite(ref myFrame, ref myPosition, mySurface);
-
-            //if (config.Get("Settings", "Reactor").ToBoolean())
-            //    DrawReactorSprite(ref myFrame, ref myPosition, mySurface);
-
-            //if (config.Get("Settings", "Ore").ToBoolean())
-            //    DrawOreSprite(ref myFrame, ref myPosition, mySurface);
-
-            //if (config.Get("Settings", "Ingot").ToBoolean())
-                DrawIngotSprite(ref myFrame, ref myPosition, mySurface);
-
-            //if (config.Get("Settings", "Component").ToBoolean())
-            //    DrawComponentSprite(ref myFrame, ref myPosition, mySurface);
-
-            //if (config.Get("Settings", "Items").ToBoolean())
-            //    DrawItemsSprite(ref myFrame, ref myPosition, mySurface);
+            DrawIngotSprite(ref myFrame, ref myPosition, mySurface);
 
             myFrame.Dispose();
         }
-
-        //void DrawTanksSprite(ref MySpriteDrawFrame frame, ref Vector2 position, IMyTextSurface surface)
-        //{
-        //    var hydrogenTanks = tanks.Where(block => block.BlockDefinition.SubtypeName.Contains("Hydrogen"));
-        //    var oxygenTanks = tanks.Where(block => !block.BlockDefinition.SubtypeName.Contains("Hydrogen"));
-
-        //    var currentHydrogen = hydrogenTanks.Count() == 0 ? 0 : hydrogenTanks.Average(block => block.FilledRatio * 100);
-        //    var totalHydrogen = hydrogenTanks.Count() == 0 ? 0 : hydrogenTanks.Sum(block => block.Capacity);
-
-        //    var currentOxygen = oxygenTanks.Count() == 0 ? 0 : oxygenTanks.Average(block => block.FilledRatio * 100);
-        //    var totalOxygen = oxygenTanks.Count() == 0 ? 0 : oxygenTanks.Sum(block => block.Capacity);
-
-        //    WriteTextSprite(ref frame, "Hydrogen Tanks", position, TextAlignment.LEFT);
-
-        //    position += newLine;
-
-        //    WriteTextSprite(ref frame, "Current:", position, TextAlignment.LEFT);
-        //    WriteTextSprite(ref frame, currentHydrogen.ToString("#0.00") + " %", position + right, TextAlignment.RIGHT);
-
-        //    position += newLine;
-
-        //    WriteTextSprite(ref frame, "Total:", position, TextAlignment.LEFT);
-        //    WriteTextSprite(ref frame, KiloFormat((int)totalHydrogen), position + right, TextAlignment.RIGHT);
-
-        //    position += newLine;
-
-        //    WriteTextSprite(ref frame, "Oxygen Tanks", position, TextAlignment.LEFT);
-
-        //    position += newLine;
-
-        //    WriteTextSprite(ref frame, "Current:", position, TextAlignment.LEFT);
-        //    WriteTextSprite(ref frame, currentOxygen.ToString("#0.00") + " %", position + right, TextAlignment.RIGHT);
-
-        //    position += newLine;
-
-        //    WriteTextSprite(ref frame, "Total:", position, TextAlignment.LEFT);
-        //    WriteTextSprite(ref frame, KiloFormat((int)totalOxygen), position + right, TextAlignment.RIGHT);
-
-        //    position += newLine + newLine;
-        //}
-
-        //void DrawSolarPanelSprite(ref MySpriteDrawFrame frame, ref Vector2 position, IMyTextSurface surface)
-        //{
-        //    var current = solarPanels.Sum(block => block.CurrentOutput);
-        //    var currentMax = solarPanels.Sum(block => block.MaxOutput);
-        //    var total = solarPanels.Sum(block => block.Components.Get<MyResourceSourceComponent>().DefinedOutput);
-
-        //    WriteTextSprite(ref frame, "Solar Panels", position, TextAlignment.LEFT);
-
-        //    position += newLine;
-
-        //    WriteTextSprite(ref frame, "Current Output:", position, TextAlignment.LEFT);
-        //    WriteTextSprite(ref frame, current.ToString("#0.00") + " MW", position + right, TextAlignment.RIGHT);
-
-        //    position += newLine;
-
-        //    WriteTextSprite(ref frame, "Current Max Output:", position, TextAlignment.LEFT);
-        //    WriteTextSprite(ref frame, currentMax.ToString("#0.00") + " MW", position + right, TextAlignment.RIGHT);
-
-        //    position += newLine;
-
-        //    WriteTextSprite(ref frame, "Total Max Output:", position, TextAlignment.LEFT);
-        //    WriteTextSprite(ref frame, total.ToString("#0.00") + " MW", position + right, TextAlignment.RIGHT);
-
-        //    position += newLine + newLine;
-        //}
-
-        //void DrawReactorSprite(ref MySpriteDrawFrame frame, ref Vector2 position, IMyTextSurface surface)
-        //{
-        //    var current = reactors.Sum(block => block.CurrentOutput);
-        //    var total = reactors.Sum(block => block.MaxOutput);
-
-        //    WriteTextSprite(ref frame, "Reactors", position, TextAlignment.LEFT);
-
-        //    position += newLine;
-
-        //    WriteTextSprite(ref frame, "Current Output:", position, TextAlignment.LEFT);
-        //    WriteTextSprite(ref frame, current.ToString("#0.00") + " MW", position + right, TextAlignment.RIGHT);
-
-        //    position += newLine;
-
-        //    WriteTextSprite(ref frame, "Max Output:", position, TextAlignment.LEFT);
-        //    WriteTextSprite(ref frame, total.ToString("#0.00") + " MW", position + right, TextAlignment.RIGHT);
-
-        //    position += newLine + newLine;
-        //}
-
-        //void DrawOreSprite(ref MySpriteDrawFrame frame, ref Vector2 position, IMyTextSurface surface)
-        //{
-        //    WriteTextSprite(ref frame, "Ores", position, TextAlignment.LEFT);
-
-        //    position += newLine;
-
-        //    foreach (var item in cargoOres)
-        //    {
-        //        MyDefinitionId.TryParse(item.Value.item.Type.TypeId, item.Value.item.Type.SubtypeId, out myDefinitionId);
-
-        //        WriteTextSprite(ref frame, myDefinitions[myDefinitionId].DisplayNameText, position, TextAlignment.LEFT);
-        //        WriteTextSprite(ref frame, KiloFormat(item.Value.amount), position + right, TextAlignment.RIGHT);
-
-        //        position += newLine;
-        //    }
-        //}
 
         void DrawIngotSprite(ref MySpriteDrawFrame frame, ref Vector2 position, IMyTextSurface surface)
         {
@@ -475,40 +280,6 @@ namespace BMC.MercenaryTSS
                 position += newLine;
             }
         }
-
-        //void DrawComponentSprite(ref MySpriteDrawFrame frame, ref Vector2 position, IMyTextSurface surface)
-        //{
-        //    WriteTextSprite(ref frame, "Components", position, TextAlignment.LEFT);
-
-        //    position += newLine;
-
-        //    foreach (var item in cargoComponents)
-        //    {
-        //        MyDefinitionId.TryParse(item.Value.item.Type.TypeId, item.Value.item.Type.SubtypeId, out myDefinitionId);
-
-        //        WriteTextSprite(ref frame, myDefinitions[myDefinitionId].DisplayNameText, position, TextAlignment.LEFT);
-        //        WriteTextSprite(ref frame, KiloFormat(item.Value.amount), position + right, TextAlignment.RIGHT);
-
-        //        position += newLine;
-        //    }
-        //}
-
-        //void DrawItemsSprite(ref MySpriteDrawFrame frame, ref Vector2 position, IMyTextSurface surface)
-        //{
-        //    WriteTextSprite(ref frame, "Items", position, TextAlignment.LEFT);
-
-        //    position += newLine;
-
-        //    foreach (var item in cargoItems)
-        //    {
-        //        MyDefinitionId.TryParse(item.Value.item.Type.TypeId, item.Value.item.Type.SubtypeId, out myDefinitionId);
-
-        //        WriteTextSprite(ref frame, myDefinitions[myDefinitionId].DisplayNameText, position, TextAlignment.LEFT);
-        //        WriteTextSprite(ref frame, KiloFormat(item.Value.amount), position + right, TextAlignment.RIGHT);
-
-        //        position += newLine;
-        //    }
-        //}
 
         static string KiloFormat(int num)
         {
