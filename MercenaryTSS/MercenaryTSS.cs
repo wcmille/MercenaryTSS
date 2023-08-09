1de607b4c66f3ba5f73440db458cd0366b87b877
Wayne Miller <wayne.c.miller@comcast.net>
2023-08-08 22:40:30 -0500

    Simple working version of ammo.

--- MercenaryTSS/MercenaryTSS.cs
+++ MercenaryTSS/MercenaryTSS.cs
@@ -17,7 +17,7 @@ using VRageMath;
 
 namespace BMC.MercenaryTSS
 {
-    class cargoItemType
+    class CargoItemType
     {
         public VRage.Game.ModAPI.Ingame.MyInventoryItem item;
         public int amount;
@@ -25,9 +25,9 @@ namespace BMC.MercenaryTSS
 
     public class ConfigIt
     {
-        public bool ConfigCheck = false;
-        MyIni config = new MyIni();
-        IMyTerminalBlock myTerminalBlock;
+        //public bool ConfigCheck = false;
+        readonly MyIni config = new MyIni();
+        readonly IMyTerminalBlock myTerminalBlock;
         public ConfigIt(IMyTerminalBlock mtb)
         {
             myTerminalBlock = mtb;
@@ -36,25 +36,21 @@ namespace BMC.MercenaryTSS
         {
             config.AddSection("Settings");
 
-            config.Set("Settings", "TextSize", "1.0");
-            config.Set("Settings", "Battery", "false");
-            config.Set("Settings", "WindTurbine", "false");
-            config.Set("Settings", "HydrogenEngine", "false");
-            config.Set("Settings", "Tanks", "false");
-            config.Set("Settings", "Solar", "false");
-            config.Set("Settings", "Reactor", "false");
-            config.Set("Settings", "Ore", "false");
-            config.Set("Settings", "Ingot", "false");
-            config.Set("Settings", "Component", "false");
-            config.Set("Settings", "Items", "false");
+            config.Set("Settings", "MyObjectBuilder_AmmoMagazine/LargeCalibreAmmo", "0");
+            config.Set("Settings", "MyObjectBuilder_AmmoMagazine/MediumCalibreAmmo", "0");
+            config.Set("Settings", "MyObjectBuilder_AmmoMagazine/AutocannonClip", "0");
+            config.Set("Settings", "MyObjectBuilder_AmmoMagazine/NATO_25x184mm", "0");
+            config.Set("Settings", "MyObjectBuilder_AmmoMagazine/LargeRailgunAmmo", "0");
+            config.Set("Settings", "MyObjectBuilder_AmmoMagazine/Missile200mm", "0");
+            config.Set("Settings", "MyObjectBuilder_AmmoMagazine/SmallRailgunAmmo", "0");
 
             config.Invalidate();
             myTerminalBlock.CustomData = config.ToString();
         }
 
-        public void LoadConfig()
+        public bool LoadConfig()
         {
-            ConfigCheck = false;
+            bool ConfigCheck = false;
 
             if (config.TryParse(myTerminalBlock.CustomData))
             {
@@ -71,6 +67,7 @@ namespace BMC.MercenaryTSS
             {
                 MyLog.Default.WriteLine("EconomySurvival.LCDInfo: Config Syntax error");
             }
+            return ConfigCheck;
         }
     }
 
@@ -81,30 +78,31 @@ namespace BMC.MercenaryTSS
     // The display name has localization support aswell, same as a block's DisplayName in SBC.
     [MyTextSurfaceScript("InventoryCounter", "BMC Inv Count")]
     public class InventoryCounter : MyTSSCommon
-    {
-        IMyTextSurface mySurface;
-        IMyTerminalBlock myTerminalBlock;
+    {      
+        readonly IMyTextSurface mySurface;
+        readonly IMyTerminalBlock myTerminalBlock;
+        private readonly IMyTerminalBlock TerminalBlock;
         Vector2 right;
         Vector2 newLine;
 
-        List<IMyInventory> inventorys = new List<IMyInventory>();
-        List<VRage.Game.ModAPI.Ingame.MyInventoryItem> inventoryItems = new List<VRage.Game.ModAPI.Ingame.MyInventoryItem>();
+        readonly List<IMyInventory> inventorys = new List<IMyInventory>();
+        readonly List<VRage.Game.ModAPI.Ingame.MyInventoryItem> inventoryItems = new List<VRage.Game.ModAPI.Ingame.MyInventoryItem>();
 
-        Dictionary<string, cargoItemType> cargoIngots = new Dictionary<string, cargoItemType>();
+        readonly Dictionary<string, CargoItemType> cargoAmmo = new Dictionary<string, CargoItemType>();
 
         VRage.Collections.DictionaryValuesReader<MyDefinitionId, MyDefinitionBase> myDefinitions;
         MyDefinitionId myDefinitionId;
         float textSize = 1.0f;
 
-        private readonly IMyTerminalBlock TerminalBlock;
         public override ScriptUpdate NeedsUpdate => ScriptUpdate.Update10; // frequency that Run() is called.
-        ConfigIt configit;
+        readonly ConfigIt configit;
 
 
         public InventoryCounter(IMyTextSurface surface, IMyCubeBlock block, Vector2 size) : base(surface, block, size)
         {
             TerminalBlock = (IMyTerminalBlock)block; // internal stored m_block is the ingame interface which has no events, so can't unhook later on, therefore this field is required.
             TerminalBlock.OnMarkForClose += BlockMarkedForClose; // required if you're gonna make use of Dispose() as it won't get called when block is removed or grid is cut/unloaded.
+            myTerminalBlock = TerminalBlock;
             mySurface = surface;
             // Called when script is created.
             // This class is instanced per LCD that uses it, which means the same block can have multiple instances of this script aswell (e.g. a cockpit with all its screens set to use this script).
@@ -123,29 +121,29 @@ namespace BMC.MercenaryTSS
             Dispose();
         }
 
-        void Draw2() // this is a custom method which is called in Run().
-        {
-            Vector2 screenSize = Surface.SurfaceSize;
-            Vector2 screenCorner = (Surface.TextureSize - screenSize) * 0.5f;
+        //void Draw2() // this is a custom method which is called in Run().
+        //{
+        //    Vector2 screenSize = Surface.SurfaceSize;
+        //    Vector2 screenCorner = (Surface.TextureSize - screenSize) * 0.5f;
 
-            var frame = Surface.DrawFrame();
+        //    var frame = Surface.DrawFrame();
 
-            // Drawing sprites works exactly like in PB API.
-            // Therefore this guide applies: https://github.com/malware-dev/MDK-SE/wiki/Text-Panels-and-Drawing-Sprites
+        //    // Drawing sprites works exactly like in PB API.
+        //    // Therefore this guide applies: https://github.com/malware-dev/MDK-SE/wiki/Text-Panels-and-Drawing-Sprites
 
-            // there are also some helper methods from the MyTSSCommon that this extends.
-            // like: AddBackground(frame, Surface.ScriptBackgroundColor); - a grid-textured background
+        //    // there are also some helper methods from the MyTSSCommon that this extends.
+        //    // like: AddBackground(frame, Surface.ScriptBackgroundColor); - a grid-textured background
 
-            // the colors in the terminal are Surface.ScriptBackgroundColor and Surface.ScriptForegroundColor, the other ones without Script in name are for text/image mode.
+        //    // the colors in the terminal are Surface.ScriptBackgroundColor and Surface.ScriptForegroundColor, the other ones without Script in name are for text/image mode.
 
-            var text = MySprite.CreateText("Hi!", "Monospace", Surface.ScriptForegroundColor, 1f, TextAlignment.LEFT);
-            text.Position = screenCorner + new Vector2(16, 16); // 16px from topleft corner of the visible surface
-            frame.Add(text);
+        //    var text = MySprite.CreateText("Hi!", "Monospace", Surface.ScriptForegroundColor, 1f, TextAlignment.LEFT);
+        //    text.Position = screenCorner + new Vector2(16, 16); // 16px from topleft corner of the visible surface
+        //    frame.Add(text);
 
-            // add more sprites and stuff
+        //    // add more sprites and stuff
 
-            frame.Dispose(); // send sprites to the screen
-        }
+        //    frame.Dispose(); // send sprites to the screen
+        //}
 
         void DrawError(Exception e)
         {
@@ -183,11 +181,6 @@ namespace BMC.MercenaryTSS
             try
             {
                 base.Run(); // do not remove
-
-                // hold L key to see how the error is shown, remove this after you've played around with it =)
-                if (MyAPIGateway.Input.IsKeyPress(VRage.Input.MyKeys.L))
-                    throw new Exception("Oh noes an error :}");
-
                 Draw();
             }
             catch (Exception e) // no reason to crash the entire game just for an LCD script, but do NOT ignore them either, nag user so they report it :}
@@ -201,76 +194,75 @@ namespace BMC.MercenaryTSS
             if (myTerminalBlock.CustomData.Length <= 0)
                 configit.CreateConfig();
 
-            configit.LoadConfig();
-
-            if (!configit.ConfigCheck)
-                return;
-
-            var myCubeGrid = myTerminalBlock.CubeGrid as MyCubeGrid;
-            var myFatBlocks = myCubeGrid.GetFatBlocks().Where(block => block.IsWorking);
-
-            inventorys.Clear();
-
-            foreach (var myBlock in myFatBlocks)
+            if (configit.LoadConfig())
             {
-                if (myBlock.HasInventory)
+
+                var myCubeGrid = myTerminalBlock.CubeGrid as MyCubeGrid;
+                var myFatBlocks = myCubeGrid.GetFatBlocks().Where(block => block.IsWorking);
+
+                inventorys.Clear();
+
+                foreach (var myBlock in myFatBlocks)
                 {
-                    for (int i = 0; i < myBlock.InventoryCount; i++)
+                    if (myBlock.HasInventory)
                     {
-                        inventorys.Add(myBlock.GetInventory(i));
+                        for (int i = 0; i < myBlock.InventoryCount; i++)
+                        {
+                            inventorys.Add(myBlock.GetInventory(i));
+                        }
                     }
                 }
-            }
 
-            cargoIngots.Clear();
+                cargoAmmo.Clear();
 
-            foreach (var inventory in inventorys)
-            {
-                if (inventory.ItemCount == 0)
-                    continue;
-
-                inventoryItems.Clear();
-                inventory.GetItems(inventoryItems);
-
-                foreach (var item in inventoryItems.OrderBy(i => i.Type.SubtypeId))
+                foreach (var inventory in inventorys)
                 {
-                    var type = item.Type.TypeId.Split('_')[1];
-                    var name = item.Type.SubtypeId;
-                    var amount = item.Amount.ToIntSafe();
+                    if (inventory.ItemCount == 0)
+                        continue;
 
-                    var myType = new cargoItemType { item = item, amount = 0 };
+                    inventoryItems.Clear();
+                    inventory.GetItems(inventoryItems);
 
-                    if (type == "Ingot")
+                    foreach (var item in inventoryItems.OrderBy(i => i.Type.SubtypeId))
                     {
-                        if (!cargoIngots.ContainsKey(name))
-                            cargoIngots.Add(name, myType);
+                        var type = item.Type.TypeId.Split('_')[1];
+                        var name = item.Type.SubtypeId;
+                        var amount = item.Amount.ToIntSafe();
 
-                        cargoIngots[name].amount += amount;
+                        var myType = new CargoItemType { item = item, amount = 0 };
+
+                        if (type == "AmmoMagazine")
+                        {
+                            if (!cargoAmmo.ContainsKey(name))
+                                cargoAmmo.Add(name, myType);
+
+                            cargoAmmo[name].amount += amount;
+                        }
                     }
                 }
+
+                var myFrame = mySurface.DrawFrame();
+                var myViewport = new RectangleF((mySurface.TextureSize - mySurface.SurfaceSize) / 2f, mySurface.SurfaceSize);
+                var myPosition = new Vector2(5, 5) + myViewport.Position;
+
+                textSize = 1.0f;//config.Get("Settings", "TextSize").ToSingle(defaultValue: 1.0f);
+                right = new Vector2(mySurface.SurfaceSize.X - 10, 0);
+                newLine = new Vector2(0, 30 * textSize);
+                myDefinitions = MyDefinitionManager.Static.GetAllDefinitions();
+
+                DrawAmmoSprite(ref myFrame, ref myPosition);
+
+                myFrame.Dispose();
             }
-
-            var myFrame = mySurface.DrawFrame();
-            var myViewport = new RectangleF((mySurface.TextureSize - mySurface.SurfaceSize) / 2f, mySurface.SurfaceSize);
-            var myPosition = new Vector2(5, 5) + myViewport.Position;
-
-            textSize = 1.0f;//config.Get("Settings", "TextSize").ToSingle(defaultValue: 1.0f);
-            right = new Vector2(mySurface.SurfaceSize.X - 10, 0);
-            newLine = new Vector2(0, 30 * textSize);
-            myDefinitions = MyDefinitionManager.Static.GetAllDefinitions();
-
-            DrawIngotSprite(ref myFrame, ref myPosition, mySurface);
-
-            myFrame.Dispose();
         }
 
-        void DrawIngotSprite(ref MySpriteDrawFrame frame, ref Vector2 position, IMyTextSurface surface)
+        void DrawAmmoSprite(ref MySpriteDrawFrame frame, ref Vector2 position)
         {
-            WriteTextSprite(ref frame, "Ingots", position, TextAlignment.LEFT);
+            WriteTextSprite(ref frame, "Ammo", position, TextAlignment.LEFT);
 
             position += newLine;
 
-            foreach (var item in cargoIngots)
+            foreach (var item in cargoAmmo)
             {
                 MyDefinitionId.TryParse(item.Value.item.Type.TypeId, item.Value.item.Type.SubtypeId, out myDefinitionId);
 
